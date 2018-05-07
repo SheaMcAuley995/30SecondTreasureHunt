75f419efae9e446ac41fc7093baedb75f3176c37
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-07 10:36:03 -0700

    Add BaseManager and BaseStructure classes

---
+++ Assets/Basebuilding/BaseManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseManager : MonoBehaviour
+{
+
+    private static BaseStructure coreStatic;
+    public static BaseStructure GetCore()
+    {
+        return coreStatic;
+    }
+
+    private static List<BaseStructure> structures = new List<BaseStructure>();
+
+    public static BaseStructure GetClosestStructure(Vector3 pos)
+    {
+        BaseStructure ret = null;
+        float minDist = float.MaxValue;
+        float thisDist;
+
+        foreach(BaseStructure strct in structures)
+        {
+            thisDist = Vector3.Distance(strct.transform.position, pos);
+            if (thisDist < minDist)
+            {
+                ret = strct;
+                minDist = thisDist;
+            }
+        }
+
+        return ret;
+    }
+
+
+    public BaseStructure core;
+
+
+
+
+    private void Awake()
+    {
+        coreStatic = core;
+    }
+
+
+}
