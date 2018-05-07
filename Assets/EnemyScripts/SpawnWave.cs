f238fd377ed4a2d7ec25134855484170345dbaf8
Shea McAuley <s179049@students.aie.edu.au>
2018-05-07 11:51:23 -0700

    wave Spawner

--- Assets/EnemyScripts/SpawnWave.cs
+++ Assets/EnemyScripts/SpawnWave.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpawnWave : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Transform enemyPrefab;
+
+    private Transform spawnPoint;
+
+    public float timeBetweenWaves = 30f;
+    private float countDown = 30f;
+
+    public Text waveCountdownText;
+
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if(countDown <= 0f)
+        {
+            StartCoroutine(SpawnNewWave());
+            countDown = timeBetweenWaves;
+        }
+        countDown -= Time.deltaTime;
+
+        waveCountdownText.text = Mathf.Round(countDown).ToString();
+    }
+
+    IEnumerator SpawnNewWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
 }
---
+++ Assets/GizmosManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class GizmosManager : MonoBehaviour {
+
+    private class CircleGizmo : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform C_transform;
+        [SerializeField]
+        private float radius;
+    }
+
+    [Header("Circle Gizmo")]
+
+    [SerializeField]
+    List<CircleGizmo> c_gizmo;
+    
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
