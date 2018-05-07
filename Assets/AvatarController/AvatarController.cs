36db5ab630f45fd0314ac739a0f7942c95ee53f4
Alexander Thomas <s179069@students.aie.edu.au>
2018-05-07 10:29:01 -0700

    Add avatar controller & base building scene

---
+++ Assets/AvatarController/AvatarController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float scrollSpeed;
+
+    public float maxHeight;
+    public float minHeight;
+
+    private Vector3 move;
+	
+
+
+	void Update () {
+        move.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        move.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        move.y = -Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+
+        transform.position += move;
+        if(transform.position.y < minHeight)
+        {
+            transform.position += Vector3.up * (minHeight - transform.position.y);
+        }
+        else if(transform.position.y > maxHeight)
+        {
+            transform.position -= Vector3.up * (transform.position.y - maxHeight);
+        }
+	}
+
+}
