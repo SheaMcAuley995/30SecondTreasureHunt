2a9f97d51a681357c1240a2d403d915ddef74018
Shea McAuley <s179049@students.aie.edu.au>
2018-05-08 10:18:21 -0700

    Dev again whoops

--- Assets/EnemyScripts/EnemyManager.cs
+++ Assets/EnemyScripts/EnemyManager.cs
@@ -20,4 +20,22 @@ public class EnemyManager : MonoBehaviour {
 
     public List<EnemyMoter> enemies = new List<EnemyMoter>();
 
+    public EnemyMoter getClosestEnemy(Vector3 pos) 
+    {
+        EnemyMoter ret = null;
+        float minDist = float.MaxValue;
+        float thisDist;
+
+        foreach(EnemyMoter enemy in enemies)
+        {
+            thisDist = Vector3.Distance(enemy.transform.position, pos);
+            if(thisDist < minDist)
+            {
+                ret = enemy;
+                minDist = thisDist;
+            }
+        }
+        return ret;
+    }
+
 }
